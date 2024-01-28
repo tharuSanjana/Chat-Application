7d8ab7974c68bd94a9a15ec4a7892ced855bac02
Tharushi <tharushisanjana28@gmail.com>
2024-01-29 01:57:50 +0530

    inite

--- src/main/java/org/example/controller/ClientFormController.java
+++ src/main/java/org/example/controller/ClientFormController.java
@@ -1,30 +1,59 @@
 package org.example.controller;
 
+import com.jfoenix.controls.JFXButton;
 import com.jfoenix.controls.JFXTextField;
 import javafx.application.Platform;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.geometry.Insets;
 import javafx.geometry.Pos;
+import javafx.scene.Node;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.GridPane;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
 import javafx.scene.paint.Color;
 import javafx.scene.text.Text;
 import javafx.scene.text.TextFlow;
+import javafx.stage.FileChooser;
+import javafx.stage.Stage;
 
 
+import java.awt.*;
 import java.io.*;
 import java.net.Socket;
 import java.net.UnknownHostException;
+import java.time.LocalDateTime;
 import java.time.LocalTime;
 import java.time.format.DateTimeFormatter;
 
 public class ClientFormController extends Thread {
-
+    private final String[] emojis = {
+            "\uD83D\uDE00", // 😀
+            "\uD83D\uDE01", // 😁
+            "\uD83D\uDE02", // 😂
+            "\uD83D\uDE03", // 🤣
+            "\uD83D\uDE04", // 😄
+            "\uD83D\uDE05", // 😅
+            "\uD83D\uDE06", // 😆
+            "\uD83D\uDE07", // 😇
+            "\uD83D\uDE08", // 😈
+            "\uD83D\uDE09", // 😉
+            "\uD83D\uDE0A", // 😊
+            "\uD83D\uDE0B", // 😋
+            "\uD83D\uDE0C", // 😌
+            "\uD83D\uDE0D", // 😍
+            "\uD83D\uDE0E", // 😎
+            "\uD83D\uDE0F", // 😏
+            "\uD83D\uDE10", // 😐
+            "\uD83D\uDE11", // 😑
+            "\uD83D\uDE12", // 😒
+            "\uD83D\uDE13"  // 😓
+    };
     @FXML
     private TextField txtMsg;
     public Socket socket;
@@ -37,38 +66,59 @@ public class ClientFormController extends Thread {
     private DataInputStream dataInputStream;
     private DataOutputStream dataOutputStream;
     public Text txtLabel;
+    @FXML
+    private AnchorPane emojiAnchorpane;
+    @FXML
+    private GridPane emojiGridpane;
 
-    public void initialize(){
+
+    public void initialize() {
         lblClientName.setText(LoginController.userName);
         //txtLabel.setText(LoginController.userName);
-
-     new Thread(new Runnable() {
-        @Override
-        public void run() {
-            try {
-                socket = new Socket("localhost", 3000);
-                System.out.println("Socket is connected with server!");
-                dataInputStream = new DataInputStream(socket.getInputStream());
-                dataOutputStream = new DataOutputStream(socket.getOutputStream());
-
-                // this.start();
-                while (socket.isConnected()){
-                    String receivingMsg = dataInputStream.readUTF();
-                    receiveMessage(receivingMsg, ClientFormController.this.VBox);
+        emojiAnchorpane.setVisible(false);
+        int btnIndex = 0;
+        for (int row = 0; row < 4; row++) {
+            for (int column = 0; column < 4; column++) {
+                if (btnIndex < emojis.length) {
+                    String emoji = emojis[btnIndex];
+                    JFXButton emojiButton = createEmojiButton(emoji);
+                    emojiGridpane.add(emojiButton, column, row);
+                    btnIndex++;
+                } else {
+                    break;
                 }
-            } catch (IOException e) {
-                e.printStackTrace();
             }
         }
-    }).start();
 
-        }
+
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    socket = new Socket("localhost", 3000);
+                    System.out.println("Socket is connected with server!");
+                    dataInputStream = new DataInputStream(socket.getInputStream());
+                    dataOutputStream = new DataOutputStream(socket.getOutputStream());
+
+                    // this.start();
+                    while (socket.isConnected()) {
+                        String receivingMsg = dataInputStream.readUTF();
+                        receiveMessage(receivingMsg, ClientFormController.this.VBox);
+                    }
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }).start();
+
+    }
 
     @FXML
     void sendBtnOnAction(ActionEvent event) throws IOException {
-       //send();
+        //send();
         sendMsg(txtMsg.getText());
     }
+
     @FXML
     void txtClientOnAction(ActionEvent event) {
         //send();
@@ -114,50 +164,50 @@ public class ClientFormController extends Thread {
         }*/
 
 
-            if (!msgToSend.isEmpty()) {
-                if (!msgToSend.matches(".*\\.(png|jpe?g|gif)$")) {
+        if (!msgToSend.isEmpty()) {
+            if (!msgToSend.matches(".*\\.(png|jpe?g|gif)$")) {
 
-                    // Construct the message including the sender's name
-                    String fullMessage = lblClientName.getText() + "-" + msgToSend;
+                // Construct the message including the sender's name
+                String fullMessage = lblClientName.getText() + "-" + msgToSend;
 
-                    HBox hBox = new HBox();
-                    hBox.setAlignment(Pos.CENTER_RIGHT);
-                    hBox.setPadding(new Insets(5, 5, 0, 10));
+                HBox hBox = new HBox();
+                hBox.setAlignment(Pos.CENTER_RIGHT);
+                hBox.setPadding(new Insets(5, 5, 0, 10));
 
-                    Text text = new Text(msgToSend);
-                    text.setStyle("-fx-font-size: 14");
-                    TextFlow textFlow = new TextFlow(text);
+                Text text = new Text(msgToSend);
+                text.setStyle("-fx-font-size: 14");
+                TextFlow textFlow = new TextFlow(text);
 
-                    // Setting style for the textFlow
-                    textFlow.setStyle("-fx-background-color: #0693e3; -fx-font-weight: bold; -fx-color: white; -fx-background-radius: 20px");
-                    textFlow.setPadding(new Insets(5, 10, 5, 10));
-                    text.setFill(Color.color(1, 1, 1));
+                // Setting style for the textFlow
+                textFlow.setStyle("-fx-background-color: #0693e3; -fx-font-weight: bold; -fx-color: white; -fx-background-radius: 20px");
+                textFlow.setPadding(new Insets(5, 10, 5, 10));
+                text.setFill(Color.color(1, 1, 1));
 
-                    hBox.getChildren().add(textFlow);
+                hBox.getChildren().add(textFlow);
 
-                    HBox hBoxTime = new HBox();
-                    hBoxTime.setAlignment(Pos.CENTER_RIGHT);
-                    hBoxTime.setPadding(new Insets(0, 5, 5, 10));
-                    String stringTime = LocalTime.now().format(DateTimeFormatter.ofPattern("HH:mm"));
-                    Text time = new Text(stringTime);
-                    time.setStyle("-fx-font-size: 8");
+                HBox hBoxTime = new HBox();
+                hBoxTime.setAlignment(Pos.CENTER_RIGHT);
+                hBoxTime.setPadding(new Insets(0, 5, 5, 10));
+                String stringTime = LocalTime.now().format(DateTimeFormatter.ofPattern("HH:mm"));
+                Text time = new Text(stringTime);
+                time.setStyle("-fx-font-size: 8");
 
-                    hBoxTime.getChildren().add(time);
+                hBoxTime.getChildren().add(time);
 
-                    VBox.getChildren().add(hBox);
-                    VBox.getChildren().add(hBoxTime);
+                VBox.getChildren().add(hBox);
+                VBox.getChildren().add(hBoxTime);
 
-                    // Sending the message
-                    dataOutputStream.writeUTF(fullMessage);
-                    dataOutputStream.flush();
+                // Sending the message
+                dataOutputStream.writeUTF(fullMessage);
+                dataOutputStream.flush();
 
-                    txtMsg.clear();
-                }
+                txtMsg.clear();
             }
         }
+    }
 
     public static void receiveMessage(String msg, VBox vBox) throws IOException {
-        if (msg.matches(".*\\.(png|jpe?g|gif)$")) {
+        /*if (msg.matches(".*\\.(png|jpe?g|gif)$")) {
             HBox hBoxName = new HBox();
             hBoxName.setAlignment(Pos.CENTER_LEFT);
             Text textName = new Text(msg.split("[-]")[0]);
@@ -210,6 +260,123 @@ public class ClientFormController extends Thread {
                     vBox.getChildren().add(hBox);
                 }
             });
+        }*/
+        String timestamp = LocalDateTime.now().format(DateTimeFormatter.ofPattern("HH:mm"));
+
+        if (msg.matches(".*\\.(png|jpe?g|gif)$")) {
+            // Handling image messages
+            // Code to display images goes here
+            HBox hBoxName = new HBox();
+            hBoxName.setAlignment(Pos.CENTER_LEFT);
+            Text textName = new Text(msg.split("[-]")[0]);
+            TextFlow textFlowName = new TextFlow(textName);
+            hBoxName.getChildren().add(textFlowName);
+
+            Image image = new Image(msg.split("[-]")[1]);
+            ImageView imageView = new ImageView(image);
+            imageView.setFitHeight(200);
+            imageView.setFitWidth(200);
+            HBox hBox = new HBox();
+            hBox.setAlignment(Pos.CENTER_LEFT);
+            hBox.setPadding(new Insets(5, 5, 5, 10));
+            hBox.getChildren().add(imageView);
+            Platform.runLater(new Runnable() {
+                @Override
+                public void run() {
+                    vBox.getChildren().add(hBoxName);
+                    vBox.getChildren().add(hBox);
+                }
+            });
+
+
+        } else {
+            // Handling text messages
+            String name = msg.split("-")[0];
+            String msgFromServer = msg.split("-")[1];
+
+            // Create a text node to display the timestamp
+            Text timeText = new Text(timestamp);
+            timeText.setStyle("-fx-font-size: 10; -fx-fill: gray;");
+
+            HBox timeBox = new HBox(timeText);
+            timeBox.setAlignment(Pos.CENTER_LEFT);
+            timeBox.setPadding(new Insets(0, 10, 0, 10));
+
+            HBox hBox = new HBox();
+            hBox.setAlignment(Pos.CENTER_LEFT);
+            hBox.setPadding(new Insets(5, 5, 5, 10));
+
+            HBox hBoxName = new HBox();
+            hBoxName.setAlignment(Pos.CENTER_LEFT);
+            Text textName = new Text(name);
+            TextFlow textFlowName = new TextFlow(textName);
+            hBoxName.getChildren().add(textFlowName);
+
+            Text text = new Text(msgFromServer);
+            TextFlow textFlow = new TextFlow(text);
+            textFlow.setStyle("-fx-background-color: #abb8c3; -fx-font-weight: bold; -fx-background-radius: 20px");
+            textFlow.setPadding(new Insets(5, 10, 5, 10));
+            text.setFill(Color.color(0, 0, 0));
+
+            hBox.getChildren().addAll(timeBox, textFlow);
+
+            Platform.runLater(() -> {
+                vBox.getChildren().add(hBoxName);
+                vBox.getChildren().add(hBox);
+            });
+        }
+    }
+
+    @FXML
+    void emojiBtnOnAction(ActionEvent event) {
+        emojiAnchorpane.setVisible(!emojiAnchorpane.isVisible());
+    }
+
+    private void emojiButtonAction(ActionEvent event) {
+        JFXButton button = (JFXButton) event.getSource();
+        txtMsg.appendText(button.getText());
+    }
+
+    private JFXButton createEmojiButton(String emoji) {
+        JFXButton button = new JFXButton(emoji);
+        button.getStyleClass().add("emoji-button");
+        button.setOnAction(this::emojiButtonAction);
+        button.setMaxSize(Double.MAX_VALUE, Double.MAX_VALUE);
+        GridPane.setFillWidth(button, true);
+        GridPane.setFillHeight(button, true);
+        button.setStyle("-fx-font-size: 15; -fx-text-fill: black; -fx-background-color: #F0F0F0; -fx-border-radius: 50");
+        return button;
+    }
+
+    @FXML
+    void imageBtnOnAction(ActionEvent event) {
+        FileDialog dialog = new FileDialog((Frame) null, "Select File to Open");
+        dialog.setMode(FileDialog.LOAD);
+        dialog.setVisible(true);
+        String file = dialog.getDirectory() + dialog.getFile();
+        dialog.dispose();
+        sendImage(file);
+        System.out.println(file + " chosen.");
+    }
+
+    private void sendImage(String msgToSend) {
+        Image image = new Image(msgToSend);
+        ImageView imageView = new ImageView(image);
+        imageView.setFitHeight(200);
+        imageView.setFitWidth(200);
+//        TextFlow textFlow = new TextFlow(imageView);
+        HBox hBox = new HBox();
+        hBox.setPadding(new Insets(5, 5, 5, 10));
+        hBox.getChildren().add(imageView);
+        hBox.setAlignment(Pos.CENTER_RIGHT);
+
+        VBox.getChildren().add(hBox);
+
+        try {
+            dataOutputStream.writeUTF(lblClientName + "-" + msgToSend);
+            dataOutputStream.flush();
+        } catch (IOException e) {
+            e.printStackTrace();
         }
     }
 }
