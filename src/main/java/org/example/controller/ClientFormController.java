8566534607708b8c3bd712231089641343c1e073
Tharushi <tharushisanjana28@gmail.com>
2024-01-30 00:46:10 +0530

    inite

--- src/main/java/org/example/controller/ClientFormController.java
+++ src/main/java/org/example/controller/ClientFormController.java
@@ -3,6 +3,8 @@ package org.example.controller;
 import com.jfoenix.controls.JFXButton;
 import com.jfoenix.controls.JFXTextField;
 import javafx.application.Platform;
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.geometry.Insets;
@@ -12,10 +14,7 @@ import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
-import javafx.scene.layout.AnchorPane;
-import javafx.scene.layout.GridPane;
-import javafx.scene.layout.HBox;
-import javafx.scene.layout.VBox;
+import javafx.scene.layout.*;
 import javafx.scene.paint.Color;
 import javafx.scene.text.Text;
 import javafx.scene.text.TextFlow;
@@ -27,9 +26,13 @@ import java.awt.*;
 import java.io.*;
 import java.net.Socket;
 import java.net.UnknownHostException;
+import java.nio.file.Files;
 import java.time.LocalDateTime;
 import java.time.LocalTime;
 import java.time.format.DateTimeFormatter;
+import java.util.LinkedList;
+import java.util.Timer;
+import java.util.TimerTask;
 
 public class ClientFormController extends Thread {
     private final String[] emojis = {
@@ -70,11 +73,13 @@ public class ClientFormController extends Thread {
     private AnchorPane emojiAnchorpane;
     @FXML
     private GridPane emojiGridpane;
-
+    @FXML
+    private ScrollPane scrollPane;
 
     public void initialize() {
         lblClientName.setText(LoginController.userName);
         //txtLabel.setText(LoginController.userName);
+
         emojiAnchorpane.setVisible(false);
         int btnIndex = 0;
         for (int row = 0; row < 4; row++) {
@@ -89,6 +94,10 @@ public class ClientFormController extends Thread {
                 }
             }
         }
+
+
+
+       // emoji();
 
 
         new Thread(new Runnable() {
@@ -125,43 +134,7 @@ public class ClientFormController extends Thread {
     }
 
     private void sendMsg(String msgToSend) throws IOException {
-       /* if (!msgToSend.isEmpty()){
-            if (!msgToSend.matches(".*\\.(png|jpe?g|gif)$")){
 
-                HBox hBox = new HBox();
-                hBox.setAlignment(Pos.CENTER_RIGHT);
-                hBox.setPadding(new Insets(5, 5, 0, 10));
-
-                Text text = new Text(msgToSend);
-                text.setStyle("-fx-font-size: 14");
-                TextFlow textFlow = new TextFlow(text);
-
-//              #0693e3 #37d67a #40bf75
-                textFlow.setStyle("-fx-background-color: #0693e3; -fx-font-weight: bold; -fx-color: white; -fx-background-radius: 20px");
-                textFlow.setPadding(new Insets(5, 10, 5, 10));
-                text.setFill(Color.color(1, 1, 1));
-
-                hBox.getChildren().add(textFlow);
-
-                HBox hBoxTime = new HBox();
-                hBoxTime.setAlignment(Pos.CENTER_RIGHT);
-                hBoxTime.setPadding(new Insets(0, 5, 5, 10));
-                String stringTime = LocalTime.now().format(DateTimeFormatter.ofPattern("HH:mm"));
-                Text time = new Text(stringTime);
-                time.setStyle("-fx-font-size: 8");
-
-                hBoxTime.getChildren().add(time);
-
-                VBox.getChildren().add(hBox);
-                VBox.getChildren().add(hBoxTime);
-
-
-                dataOutputStream.writeUTF(lblClientName + "-" + msgToSend);
-                dataOutputStream.flush();
-
-                txtMsg.clear();
-            }
-        }*/
 
 
         if (!msgToSend.isEmpty()) {
@@ -207,60 +180,7 @@ public class ClientFormController extends Thread {
     }
 
     public static void receiveMessage(String msg, VBox vBox) throws IOException {
-        /*if (msg.matches(".*\\.(png|jpe?g|gif)$")) {
-            HBox hBoxName = new HBox();
-            hBoxName.setAlignment(Pos.CENTER_LEFT);
-            Text textName = new Text(msg.split("[-]")[0]);
-            TextFlow textFlowName = new TextFlow(textName);
-            hBoxName.getChildren().add(textFlowName);
 
-            Image image = new Image(msg.split("[-]")[1]);
-            ImageView imageView = new ImageView(image);
-            imageView.setFitHeight(200);
-            imageView.setFitWidth(200);
-            HBox hBox = new HBox();
-            hBox.setAlignment(Pos.CENTER_LEFT);
-            hBox.setPadding(new Insets(5, 5, 5, 10));
-            hBox.getChildren().add(imageView);
-            Platform.runLater(new Runnable() {
-                @Override
-                public void run() {
-                    vBox.getChildren().add(hBoxName);
-                    vBox.getChildren().add(hBox);
-                }
-            });
-
-        } else {
-            String name = msg.split("-")[0];
-            String msgFromServer = msg.split("-")[1];
-
-            HBox hBox = new HBox();
-            hBox.setAlignment(Pos.CENTER_LEFT);
-            hBox.setPadding(new Insets(5, 5, 5, 10));
-
-            HBox hBoxName = new HBox();
-            hBoxName.setAlignment(Pos.CENTER_LEFT);
-            Text textName = new Text(name);
-            TextFlow textFlowName = new TextFlow(textName);
-            hBoxName.getChildren().add(textFlowName);
-
-            Text text = new Text(msgFromServer);
-            TextFlow textFlow = new TextFlow(text);
-            textFlow.setStyle("-fx-background-color: #abb8c3; -fx-font-weight: bold; -fx-background-radius: 20px");
-            textFlow.setPadding(new Insets(5, 10, 5, 10));
-            text.setFill(Color.color(0, 0, 0));
-
-
-            hBox.getChildren().add(textFlow);
-
-            Platform.runLater(new Runnable() {
-                @Override
-                public void run() {
-                    vBox.getChildren().add(hBoxName);
-                    vBox.getChildren().add(hBox);
-                }
-            });
-        }*/
         String timestamp = LocalDateTime.now().format(DateTimeFormatter.ofPattern("HH:mm"));
 
         if (msg.matches(".*\\.(png|jpe?g|gif)$")) {
@@ -357,10 +277,11 @@ public class ClientFormController extends Thread {
         dialog.dispose();
         sendImage(file);
         System.out.println(file + " chosen.");
+
     }
 
     private void sendImage(String msgToSend) {
-        Image image = new Image(msgToSend);
+      /*  Image image = new Image(msgToSend);
         ImageView imageView = new ImageView(image);
         imageView.setFitHeight(200);
         imageView.setFitWidth(200);
@@ -378,8 +299,57 @@ public class ClientFormController extends Thread {
         } catch (IOException e) {
             e.printStackTrace();
         }
+    }*/
+       /* File file = new File(msgToSend);
+        String imageUrl = file.toURI().toString(); // Convert file path to a URL string
+        Image image = new Image(imageUrl);
+
+        ImageView imageView = new ImageView(image);
+        imageView.setFitHeight(200);
+        imageView.setFitWidth(200);
+
+        HBox hBox = new HBox();
+        hBox.setPadding(new Insets(5, 5, 5, 10));
+        hBox.getChildren().add(imageView);
+        hBox.setAlignment(Pos.CENTER_RIGHT);
+
+        VBox.getChildren().add(hBox);
+
+        try {
+            dataOutputStream.writeUTF(lblClientName.getText() + "-" + imageUrl); // Sending the URL, not the file path
+            dataOutputStream.flush();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }*/
+
+        File file = new File(msgToSend);
+        String imageUrl = file.toURI().toString(); // Convert file path to a URL string
+        Image image = new Image(imageUrl);
+
+        ImageView imageView = new ImageView(image);
+        imageView.setFitHeight(200);
+        imageView.setFitWidth(200);
+
+        HBox hBox = new HBox();
+        hBox.setPadding(new Insets(5, 5, 5, 10));
+        hBox.getChildren().add(imageView);
+        hBox.setAlignment(Pos.CENTER_RIGHT);
+
+        VBox.getChildren().add(hBox);
+
+        try {
+            dataOutputStream.writeUTF(lblClientName.getText() + "-" + imageUrl); // Sending the URL, not the file path
+            dataOutputStream.flush();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+
+
     }
+
 }
+
 
 
 
